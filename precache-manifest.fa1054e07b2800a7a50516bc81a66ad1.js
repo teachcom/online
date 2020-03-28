0704b2bda1f789d1537c8f874d76b2e5904ff96f
akopadung <akopadung@gmail.com>
2020-03-28 23:31:17 +0700

    Updates

---
+++ precache-manifest.fa1054e07b2800a7a50516bc81a66ad1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6e4bf4611db24f09cf37b3032ce5c7",
+    "url": "/online/index.html"
+  },
+  {
+    "revision": "94a3baf4df0627265b1e",
+    "url": "/online/static/css/main.32a38855.chunk.css"
+  },
+  {
+    "revision": "5ba56110f38653a48d8a",
+    "url": "/online/static/js/2.0d6aba3e.chunk.js"
+  },
+  {
+    "revision": "91dbce2febfc227edf628f0fd0b15354",
+    "url": "/online/static/js/2.0d6aba3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a3baf4df0627265b1e",
+    "url": "/online/static/js/main.358131e1.chunk.js"
+  },
+  {
+    "revision": "e62d6714b3ccb24fc640",
+    "url": "/online/static/js/runtime-main.f84a8108.js"
+  },
+  {
+    "revision": "8eaa53ab12737df476ec37173e40ac6e",
+    "url": "/online/static/media/about.8eaa53ab.jpg"
+  },
+  {
+    "revision": "fb95d7130f66e3792136167b39aed951",
+    "url": "/online/static/media/img_bg_slide0.fb95d713.jpg"
+  },
+  {
+    "revision": "2e01b436a72d0a351efcaf820b98cfa6",
+    "url": "/online/static/media/img_bg_slide_1.2e01b436.JPG"
+  },
+  {
+    "revision": "5bbce33e2725f3d5888fc2ac52ee54f1",
+    "url": "/online/static/media/img_bg_slide_2.5bbce33e.jpg"
+  }
+]);
